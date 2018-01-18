15a608e609342243ab339997d1d57de387241a39
stephen <stephenrs7@gmail.com>
2018-01-18 14:34:29 -0800

    firsT

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id INTEGER(10) NOT NULL AUTO_INCREMENT,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY (id)
+);
+
+SELECT * FROM burgers;
---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Double');
+INSERT INTO burgers (burger_name) VALUES ('Chicken Sandwich');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Sausage Egg McMuffin', true);
