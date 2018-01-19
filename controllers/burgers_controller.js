a5d31ed436ed50c45c324652df4304e810831a68
stephen <stephenrs7@gmail.com>
2018-01-18 19:09:51 -0800

    third

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,7 +3,7 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (cat.js) to use its database functions.
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
@@ -36,19 +36,6 @@ router.put("/api/burgers/:id", function(req, res) {
     devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/cats/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  cat.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
       // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Big Kahuna Burger');
 INSERT INTO burgers (burger_name) VALUES ('Double Double');
 INSERT INTO burgers (burger_name) VALUES ('Chicken Sandwich');
 INSERT INTO burgers (burger_name, devoured) VALUES ('Sausage Egg McMuffin', true);
