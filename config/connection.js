67239435bbd226bc0e31fd5951375da9a9c99c09
stephen <stephenrs7@gmail.com>
2018-01-18 23:56:02 -0800

    sixth

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@
 var mysql = require("mysql");
 var connection;
 
-if (process.env.JAWSDB_UR) {
+if (process.env.JAWSDB_URL) {
     connection.mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
